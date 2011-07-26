f0bc54fc6d502bd7a727da1ac4ad51602c376c60
Alex Harin <aharin@thoughtworks.com>
2011-07-26 10:56:11 +0100

    Adding licensing information

--- inproctester-htmlunit/src/main/java/com/thoughtworks/inproctester/htmlunit/InProcessWebConnection.java
+++ inproctester-htmlunit/src/main/java/com/thoughtworks/inproctester/htmlunit/InProcessWebConnection.java
@@ -1,3 +1,11 @@
+/*
+ * (c) 2011 ThoughtWorks Ltd
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the Apache License, Version 2.0
+ * a copy of which has been included with this distribution in the LICENSE.txt file.
+ *
+ */
 package com.thoughtworks.inproctester.htmlunit;
 
 import com.gargoylesoftware.htmlunit.*;
