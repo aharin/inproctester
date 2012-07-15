c3a75afb9330df2754cbd3fc527f24bf48d57e0c
Toby Clemson <tobyclemson@gmail.com>
2012-07-15 12:54:27 -0700

    Extending UrlHelper tests to consider encoded URI components and making the relevant fixes.

--- inproctester-jetty/src/test/java/com/thoughtworks/inproctester/jetty/UrlHelperTest.java
+++ inproctester-jetty/src/test/java/com/thoughtworks/inproctester/jetty/UrlHelperTest.java
@@ -13,12 +13,16 @@ public class UrlHelperTest {
 
     @Test
     public void shouldExtractPathWithQueryAndFragment() throws URISyntaxException {
-        assertThat(UrlHelper.getRequestPath(new URI("http://localhost:8902/status?ln=test#help")), is("/status?ln=test#help"));
+        assertThat(UrlHelper.getRequestPath(new URI("http://localhost:8902/status?ln=test%20value#help%20page")), is("/status?ln=test%20value#help%20page"));
     }
 
     @Test
     public void shouldExtractPathWithQuery() throws URISyntaxException {
-        assertThat(UrlHelper.getRequestPath(new URI("http://localhost:8902/status?ln=test")), is("/status?ln=test"));
+        assertThat(UrlHelper.getRequestPath(new URI("http://localhost:8902/status?ln=test%20value")), is("/status?ln=test%20value"));
     }
 
+    @Test
+    public void shouldCorrectlyEscapeSpacesInPath() throws Exception {
+        assertThat(UrlHelper.getRequestPath(new URI("http://localhost:8902/some%20path?ln=test")), is("/some%20path?ln=test"));
+    }
 }
